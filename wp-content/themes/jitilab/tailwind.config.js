1a10b131035373dd964f414cd5fa2d29e7998496
fuadhn <fhadinugroho@gmail.com>
2024-04-30 11:24:16 +0700

    Instalasi TailwindCSS & Konfigurasi Awal
    Digunakan untuk generate css untuk tampilan website.

---
+++ wp-content/themes/jitilab/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'jtl-',
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'jtlprimary': '#0e0a38',
+        'jtlsecondary': '#ff630e',
+        'jtllight': '#dedede',
+        'jtldark': '#130f40',
+        'jtlgray': '#696969'
+      },
+      fontFamily: {
+        'urbanist': 'Urbanist',
+        'rubik': 'Rubik'
+      }
+    },
+  },
+  plugins: [],
+}
