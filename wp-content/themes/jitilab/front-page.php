d0c3b9eba5745789ec64d1b81ddddd35b32cf303
fuadhn <fhadinugroho@gmail.com>
2024-05-01 19:10:05 +0700

    Halaman Depan: Tentang Kami

--- wp-content/themes/jitilab/front-page.php
+++ wp-content/themes/jitilab/front-page.php
@@ -9,6 +9,17 @@
  * @since JITILAB 1.0
  */
 
+  // Constanta
+  $homepage_ID = get_option('page_on_front');
+
+  // Fields
+  $hero_background = get_field('hero_background', $homepage_ID);
+  $hero_label = get_field('hero_label', $homepage_ID);
+  $hero_title = get_field('hero_title', $homepage_ID);
+  $hero_description = get_field('hero_description', $homepage_ID);
+  $hero_link = get_field('hero_link', $homepage_ID);
+  $hero_image = get_field('hero_image', $homepage_ID);
+
   get_header();
 
   while(have_posts()) {
@@ -18,7 +29,7 @@
   <!-- Main -->
   <main id="jtlMain" class="jtl-bg-white">
     <!-- Hero -->
-    <section data-image="<?php echo esc_url(get_template_directory_uri() . '/dist/img/campus.webp'); ?>" class="jtl-lazyload jtl-hero-wrap">
+    <section data-image="<?php echo esc_url($hero_background); ?>" class="jtl-lazyload jtl-hero-wrap">
       <div class="jtl-hero-overlay"></div>  
 
       <div class="jtl-custom-container jtl-relative jtl-z-[2]">
@@ -26,15 +37,15 @@
           <div class="jtl-hero-content">
             <div class="jtl-space-y-8 xl:jtl-space-y-12">
               <div class="jtl-space-y-4">
-                <h1 class="jtl-label">Tentang Kami</h1>
-                <h2 class="jtl-title">JITILAB University adalah kampus terbaik untuk karir dan masa depan Anda.</h2>
-                <p class="jtl-desc">Universitas ini didirikan pada tahun 1990. Terdapat 30+ fakultas dengan kampus unggulan. Semua guru berpengalaman tinggi.</p>
+                <h1 class="jtl-label"><?php echo esc_html($hero_label); ?></h1>
+                <h2 class="jtl-title"><?php echo esc_html($hero_title); ?></h2>
+                <p class="jtl-desc"><?php echo esc_html($hero_description); ?></p>
               </div>
 
               <div>
-                <a href="#">
+                <a href="<?php echo esc_url($hero_link); ?>">
                   <button type="button" class="jtl-button primary">
-                    <span class="jtl-text">Pelajari Lebih</span>
+                    <span class="jtl-text"><?php echo __('Pelajari Lebih', 'jitilab'); ?></span>
                     <i class="fa-solid fa-arrow-right jtl-icon jtl-animate-sliding"></i>
                   </button>
                 </a>
@@ -44,10 +55,10 @@
             
           <div class="jtl-hero-img jtl-hidden lg:jtl-block">
             <div class="jtl-relative">
-              <img width="632" height="947" src="" data-image="<?php echo esc_url(get_template_directory_uri() . '/dist/img/students.webp'); ?>" alt="" class="jtl-img jtl-lazyload" />
+              <img width="632" height="947" src="" data-image="<?php echo esc_url($hero_image); ?>" alt="" class="jtl-img jtl-lazyload" />
 
               <noscript>
-                <img width="632" height="947" src="<?php echo esc_url(get_template_directory_uri() . '/dist/img/students.webp'); ?>" alt="" class="jtl-img" />
+                <img width="632" height="947" src="<?php echo esc_url($hero_image); ?>" alt="" class="jtl-img" />
               </noscript>
             </div>
           </div>
